df8e52a86801841d404d45bf332b9f9a403ef17d
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-06 22:54:55 +0300

    Function editting

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,6 +6,8 @@ from typing import Union, Mapping, Any, TypeVar
 
 
 T = TypeVar("T")
+
+
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None
                      ) -> Union[Any, T]:
