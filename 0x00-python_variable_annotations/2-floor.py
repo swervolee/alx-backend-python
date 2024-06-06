a2f0ef56df4a6d6ea9fb3d235b5843281518c27c
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-06 16:35:18 +0300

    Floors a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Python annotation
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    A function to round off using floor
+    """
+    return math.floor(n)
