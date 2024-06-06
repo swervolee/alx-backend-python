aba7c6763b7012cfa65002e834927ee4f6a71a3b
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-06 17:47:22 +0300

    Annotated tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python annotations
+"""
+from typing import Union, Tuple
+
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Annotation in python
+    """
+    return (k, v ** 2)
