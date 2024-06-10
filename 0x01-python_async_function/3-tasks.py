b1597dff1d3d8005c7a79e7f109ef83b07ee24e3
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-10 20:49:04 +0300

    Async Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,15 +3,20 @@
 PYTHON ASYNC
 """
 import asyncio
-from typing import Callable
-
+from typing import Any
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Callable[[int], float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Return an async function
+    Creates an asyncio task for the wait_random coroutine with a given max_delay.
+
+    Args:
+        max_delay (int): The maximum delay for the wait_random coroutine.
+
+    Returns:
+        asyncio.Task: An asyncio task object for the wait_random coroutine.
     """
     loop = asyncio.get_event_loop()
     task = loop.create_task(wait_random(max_delay))
