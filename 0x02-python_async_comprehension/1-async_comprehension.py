dffa16bd9178fda474a3a0377a22536fe4bb4dd9
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-11 15:40:44 +0300

    Python Async

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+PYTHON ASYNC
+"""
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    PYTHON ASYNC
+    """
+    return [i async for i in async_generator()]
