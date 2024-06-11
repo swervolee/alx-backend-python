faa3c21ae5bafc6457ea5a2d5b2d17c9f378d6da
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-11 15:22:00 +0300

    Initial commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,6 +4,7 @@ PYTHON ASYNC
 """
 import random
 from typing import Generator
+import asyncio
 
 
 async def async_generator() -> Generator[int, None, None]:
@@ -11,4 +12,5 @@ async def async_generator() -> Generator[int, None, None]:
     Generator
     """
     for i in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
